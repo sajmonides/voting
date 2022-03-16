34b145a593875d77f69ebcd8a0ddf9a0833bb127
Simon P <spietrzyba@cenero.com>
2022-03-15 23:53:44 -0400

    .

--- voting/src/Voting/VotingProcedure.js
+++ voting/src/Voting/VotingProcedure.js
@@ -1,21 +1,26 @@
 
 
 const Run = async (candidates, voterCount) => {
+
+    const getRandomNumber = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
     let voters = [];
     const candidateCount = candidates.length;
     for (var z = 0; z < candidates.length; z++) {
         candidates[z].Id = z;
     }
+
     for (var i = 1; i <= voterCount; i++) {
         let candidateIds = [];
         let thisVoter = [];
         candidates.map((cand) => candidateIds.push(cand.Id));
 
         for (var k = 1; k <= candidateCount; k++) {
-            const min = 0;
-            const max = Math.floor(candidateIds.length - 1);
-            const rand = Math.floor(Math.random() * (max - min + 1)) + min;
-            //const rand = getRandomNumber(0, candidateIds.length - 1);
+            const rand = getRandomNumber(0, candidateIds.length - 1);
             const thisVote = candidateIds[rand];
             thisVoter.push(thisVote);
             candidateIds = candidateIds.filter(c => c != thisVote);
@@ -33,10 +38,6 @@ const Run = async (candidates, voterCount) => {
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
 
 export default Run;
